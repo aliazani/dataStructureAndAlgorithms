db72401b9805476c9f6e5bf06c21228e536ddc7a
Mohammadali Azani <aliazn2013@yahoo.com>
2021-03-08 14:48:54 +0330

    defualt behavior of linkedlist

--- data_structure_and_algorithms/src/LinkedList/LinkedListDemo.java
+++ data_structure_and_algorithms/src/LinkedList/LinkedListDemo.java
@@ -1,2 +1,15 @@
-package LinkedList;public class LinkedListDemo {
+package LinkedList;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        System.out.println("Linked List");
+    }
+
+    public static void showLinkedListDefaultImplementation() {
+        var linkedList = new LinkedList<Integer>();
+        linkedList.add(10);
+
+    }
 }
