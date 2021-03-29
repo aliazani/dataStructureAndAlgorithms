f6e3c4d789f91daaeeeb22434d217842663d914e
Mohammadali Azani <aliazn2013@yahoo.com>
2021-03-29 11:20:59 +0430

    Implement traverse using recursion and check euqality of 2 trees, check if it is binary tree or not

--- data_structure_and_algorithms/src/NonLinear/BinaryTree/BinaryTree.java
+++ data_structure_and_algorithms/src/NonLinear/BinaryTree/BinaryTree.java
@@ -27,12 +27,6 @@ public class BinaryTree<T extends Comparable<T>> {
         }
     }
 
-    @Override
-    public String toString() {
-        return root.toString();
-
-    }
-
     public boolean find(T item) {
         var current = root;
 
@@ -45,6 +39,122 @@ public class BinaryTree<T extends Comparable<T>> {
                 return true;
         }
         return false;
+    }
+
+    public void swapRoot() {
+        var temp = root.leftChild;
+        root.leftChild = root.rightChild;
+        root.rightChild = temp;
+    }
+
+    public boolean isBinarySearchTree(T min, T max) {
+        return isBinarySearchTree(root, min, max);
+    }
+
+    private boolean isBinarySearchTree(Node<T> root, T min, T max) {
+        if (isEmpty(root))
+            return true;
+        if (root.value.compareTo(min) <= 0 || root.value.compareTo(max) >= 0)
+            // root - max < 0
+            // root - min > 0
+            return false;
+
+        return isBinarySearchTree(root.leftChild, min, root.value) &&
+                isBinarySearchTree(root.rightChild, root.value, max);
+    }
+
+    public void traversePreOrder() {
+        traversePreOrder(root);
+    }
+
+    private void traversePreOrder(Node<T> root) {
+        if (isEmpty(root))
+            return;
+        System.out.println(root.value);
+        traversePreOrder(root.leftChild);
+        traversePreOrder(root.rightChild);
+    }
+
+    public void traversePostOrder() {
+        traversePostOrder(root);
+    }
+
+    private void traversePostOrder(Node<T> root) {
+        if (isEmpty(root))
+            return;
+        traversePostOrder(root.leftChild);
+        traversePostOrder(root.rightChild);
+        System.out.println(root.value);
+    }
+
+    public void traverseInOrder() {
+        traverseInOrder(root);
+    }
+
+    private void traverseInOrder(Node<T> root) {
+        if (isEmpty(root))
+            return;
+        traverseInOrder(root.leftChild);
+        System.out.println(root.value);
+        traverseInOrder(root.rightChild);
+    }
+
+    public int height() {
+        return height(root);
+    }
+
+    private int height(Node<T> root) {
+        if (isEmpty((Node<T>) root))
+            return -1;
+        if (isLeaf(root))
+            return 0;
+        return 1 + Math.max(
+                height(root.leftChild),
+                height(root.rightChild));
+    }
+
+    public boolean equals(BinaryTree<T> other) {
+        if (other == null)
+            return false;
+        return equals(root, other.root);
+    }
+
+    private boolean equals(Node<T> first, Node<T> second) {
+        if (first == null && second == null)
+            return true;
+        if (first != null && second != null)
+            return first.value == second.value
+                    && equals(first.leftChild, second.leftChild)
+                    && equals(first.rightChild, second.rightChild);
+        return false;
+    }
+
+    public T minimum() {
+        return minimum(root);
+    }
+
+    private T minimum(Node<T> root) {
+        if (isLeaf(root))
+            return root.value;
+        T minValue = null;
+        var left = minimum(root.leftChild);
+        var right = minimum(root.rightChild);
+        minValue = (right.compareTo(left) < 0) ? right : left;
+        minValue = root.value.compareTo(minValue) < 0 ? root.value : minValue;
+        return minValue;
+    }
+
+    private boolean isLeaf(Node<T> root) {
+        return root.rightChild == null && root.leftChild == null;
+    }
+
+    private boolean isEmpty(Node<T> root) {
+        return root == null;
+    }
+
+    @Override
+    public String toString() {
+        return root.toString();
     }
 
     private class Node<T> {
--- data_structure_and_algorithms/src/NonLinear/BinaryTree/BinaryTreeDemo.java
+++ data_structure_and_algorithms/src/NonLinear/BinaryTree/BinaryTreeDemo.java
@@ -1,16 +1,39 @@
 package NonLinear.BinaryTree;
 
 public class BinaryTreeDemo {
-    public static void main(String[] args){
-        BinaryTree<Integer> tree = new BinaryTree<>(10);
-        tree.insert(20);
-        tree.insert(3);
-        tree.insert(12);
-        tree.insert(8);
-        var res = tree.find(10);
+    public static void main(String[] args) {
+        BinaryTree<Integer> tree1 = new BinaryTree<>(10);
+        tree1.insert(20);
+        tree1.insert(12);
+        tree1.insert(30);
+        tree1.insert(4);
+        tree1.insert(8);
+        tree1.insert(3);
+        var res = tree1.find(10);
         System.out.println(res);
-        var res2 = tree.find(23);
+        var res2 = tree1.find(23);
         System.out.println(res2);
-        System.out.println(tree);
+        System.out.println(tree1);
+        System.out.println("Post Order:");
+        tree1.traversePostOrder();
+        System.out.println("Pre Order:");
+        tree1.traversePreOrder();
+        System.out.println("In order:");
+        tree1.traverseInOrder();
+        System.out.println("height: " + tree1.height());
+        System.out.println("Minimum: " + tree1.minimum());
+
+        BinaryTree<Integer> tree2 = new BinaryTree<>(10);
+        tree2.insert(20);
+        tree2.insert(12);
+        tree2.insert(30);
+        tree2.insert(4);
+        tree2.insert(8);
+        tree2.insert(3);
+        System.out.println("equals: " + tree1.equals(tree2));
+
+        System.out.println("Binary Tree: " + tree1.isBinarySearchTree(Integer.MIN_VALUE, Integer.MAX_VALUE));
+        tree1.swapRoot();
+        System.out.println("Binary Tree: " + tree1.isBinarySearchTree(Integer.MIN_VALUE, Integer.MAX_VALUE));
     }
 }
