f9dd4fc8956e19abefa74f29fe8addfc19839b47
Mohammadali Azani <aliazn2013@yahoo.com>
2021-05-15 13:00:34 +0430

    remove method for trie

---
+++ data_structure_and_algorithms/src/NonLinear/Trie/Trie2.java
@@ -0,0 +1,117 @@
+package NonLinear.Trie;
+
+import java.util.HashMap;
+
+public class Trie2 {
+
+    private class Node {
+        private char value;
+        private HashMap<Character, Node> children = new HashMap<>();
+        private boolean isEndOfWord;
+
+        public Node(char value) {
+            this.value = value;
+        }
+
+        public boolean hasChild(char ch) {
+            return children.containsKey(ch);
+        }
+
+        public void addChild(char ch) {
+            children.put(ch, new Node(ch));
+        }
+
+        public Node getChild(char ch) {
+            return children.get(ch);
+        }
+
+        public Node[] getChildren() {
+            return children.values().toArray(new Node[0]);
+        }
+
+        public boolean hasChildren() {
+            return !children.isEmpty();
+        }
+
+        public void removeChild(char ch) {
+            children.remove(ch);
+        }
+
+        @Override
+        public String toString() {
+            return "value=" + value;
+        }
+
+    }
+
+    private Node root = new Node(' ');
+
+    public void insert(String word) {
+        var current = root;
+
+        for (var ch : word.toCharArray()) {
+            if (!current.hasChild(ch))
+                current.addChild(ch);
+            current = current.getChild(ch);
+        }
+        current.isEndOfWord = true;
+    }
+
+    public boolean contains(String word) {
+        if (word == null)
+            return false;
+
+        var current = root;
+
+        for (var ch : word.toCharArray()) {
+            if (!current.hasChild(ch))
+                return false;
+            current = current.getChild(ch);
+        }
+        return current.isEndOfWord;
+    }
+
+    public void preOrderTraverse() {
+        preOrderTraverse(root);
+    }
+
+    private void preOrderTraverse(Node root) {
+        System.out.println(root.value);
+
+        for (var child : root.getChildren())
+            preOrderTraverse(child);
+    }
+
+    public void postOrderTraverse() {
+        postOrderTraverse(root);
+    }
+
+    private void postOrderTraverse(Node root) {
+        for (var child : root.getChildren())
+            postOrderTraverse(child);
+
+        System.out.println(root.value);
+    }
+
+    public void remove(String word) {
+        if (word == null)
+            return;
+        remove(root, word, 0);
+    }
+
+    private void remove(Node root, String word, int index) {
+        if (index == word.length()) {
+            root.isEndOfWord = false;
+            return;
+        }
+
+        var ch = word.charAt(index);
+        var child = root.getChild(ch);
+        if (child == null)
+            return;
+
+        remove(child, word, index + 1);
+        if (!child.hasChildren() && !child.isEndOfWord)
+            root.removeChild(ch);
+    }
+}
--- data_structure_and_algorithms/src/NonLinear/Trie/TrieTest.java
+++ data_structure_and_algorithms/src/NonLinear/Trie/TrieTest.java
@@ -1,20 +1,63 @@
 package NonLinear.Trie;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TrieTest {
     private Trie trie;
+    private Trie2 trie2;
 
     @Before
     public void setUp() {
         trie = new Trie();
+        trie2 = new Trie2();
     }
 
     @Test
-    public void insertTest() {
+    public void insertTestForArray() {
         trie.insert("cat");
         trie.insert("can");
         System.out.println(trie);
+    }
+
+    @Test
+    public void insertTestForHashMap() {
+        trie2.insert("cat");
+        trie2.insert("can");
+        System.out.println(trie2);
+    }
+
+    @Test
+    public void lookup() {
+        trie2.insert("canada");
+        trie2.insert("cat");
+        Assert.assertTrue(trie2.contains("canada"));
+        Assert.assertFalse(trie2.contains("can"));
+        Assert.assertTrue(trie2.contains("cat"));
+    }
+
+    @Test
+    public void preOrderTraverse() {
+        trie2.insert("can");
+        trie2.insert("cat");
+        trie2.insert("car");
+        trie2.insert("canada");
+        trie2.preOrderTraverse();
+    }
+
+    @Test
+    public void postOrderTraverse() {
+        trie2.insert("can");
+        trie2.postOrderTraverse();
+    }
+
+    @Test
+    public void delete() {
+        trie2.insert("can");
+        trie2.insert("canada");
+        trie2.remove("canada");
+        Assert.assertFalse(trie2.contains("canada"));
+        Assert.assertTrue(trie2.contains("can"));
     }
 }
